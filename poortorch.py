7fe9224c8d617baf3b40295b7f05f573d9d47630
Tavish Mankash <tavish.mankash@gmail.com>
2025-07-19 00:20:28 +0530

    Added tensor creation method

--- poortorch.py
+++ poortorch.py
@@ -1,6 +1,6 @@
 import numpy as np
 import typing
-from typing import List, Union, Optional
+from typing import List, Union, Optional, Callable
 from typing import TypeVar, Generic
 import math
 
@@ -18,29 +18,41 @@ class poortorch:
 
     dtype = TypeVar("dtype", "poortorch.int8", "poortorch.int16", "poortorch.int32", "poortorch.int64",
                            "poortorch.float16", "poortorch.float32", "poortorch.float64")
+    
+    # Tensor Creation
+    def zeros(shape: tuple[int], dtype: "poortorch.dtype" = None) -> "poortorch.tensor":
+        return poortorch.tensor.helper._create_per_value_independent_tensor(shape, lambda: 0, dtype)
 
     class tensor:
-        def __init__(self, xl: Union[list, float, int], dtype: "poortorch.dtype" = None):
+        def __init__(self, xl: Union[list, float, int], dtype: "poortorch.dtype" = None, manual_creation_dict: dict = None):
             if isinstance(xl, (int, float)): # Scalars
                 self.shape = []
                 self.stride = []
                 self.dtype = dtype if dtype else (poortorch.float32 if isinstance(xl, float) else poortorch.int64)
                 self.__storage__ = self.dtype(xl)
             elif isinstance(xl, list): 
-                # Storage
-                self.__storage__ = []
-                self.helper._flatten_list(xl)
-
-                # Shape and Deciding dtype: If shape is None, int64 or float32 is selected depending on data. Otherwise, specified dtype is used.
-                self.dtype = dtype
-                if not self.dtype: 
-                    self.shape = tuple(self.helper._shape_iterable(xl)[::-1]) # Sets dtype to flaot32 if float is encountered
-                    if not self.dtype: self.dtype = poortorch.int64           # Sets dtype to int64 if float is not encountered
+                if manual_creation_dict: 
+                    # If a tensor is created outside of the list to tensor implementation, only a flat list, shape and dtype are required. 
+                    # Stride is calculated automatically. __storage__ must be list[int, float]. They are converted to the specified dtype.
+                    if dtype or xl: raise Exception("dtype must be None and input list provided must be [] if using manual dict creation 😔")
+                    self.__storage__ = manual_creation_dict["__storage__"]
+                    self.dtype = manual_creation_dict["dtype"]
+                    self.shape = manual_creation_dict["shape"]
                 else:
-                    self.shape = tuple(self.helper._shape_iterable(xl)[::-1]) # Throws error if float is encountered in int dtypes. It's okay to encounter ints in float dtypes.
+                    # Storage
+                    self.__storage__ = []
+                    poortorch.tensor.helper._flatten_list(self, xl)
+
+                    # Shape and Deciding dtype: If shape is None, int64 or float32 is selected depending on data. Otherwise, specified dtype is used.
+                    self.dtype = dtype
+                    if not self.dtype: 
+                        self.shape = tuple(poortorch.tensor.helper._shape_iterable(self, xl)[::-1]) # Sets dtype to flaot32 if float is encountered
+                        if not self.dtype: self.dtype = poortorch.int64           # Sets dtype to int64 if float is not encountered
+                    else:
+                        self.shape = tuple(poortorch.tensor.helper._shape_iterable(self, xl)[::-1]) # Throws error if float is encountered in int dtypes. It's okay to encounter ints in float dtypes.
 
                 # Strides
-                self.stride = [None for _ in range(self.shape)]
+                self.stride = [None for _ in range(len(self.shape))]
                 for i in reversed(range(len(self.shape) - 1)):
                     self.stride[i] = self.shape[i + 1] * self.stride[i + 1]
                 
@@ -49,7 +61,7 @@ class poortorch:
             else:
                 raise Exception("Tensor can only be created from int, float or lists 😔")
             
-        def __str__(self):
+        def __str__(self) -> str:
             if len(self.shape) == 0:
                 return f"poortorch.tensor({self.__storage__}, dtype={self.dtype.__name__})"
             
@@ -95,13 +107,13 @@ class poortorch:
             formatted_data = format_tensor(self.__storage__, self.shape)
             return f"poortorch.tensor({formatted_data}, dtype={self.dtype.__name__})"
         
-        def __int__(self):
+        def __int__(self) -> int:
             if len(self.shape) != 0:
                 raise Exception("Cannot convert non-scalar tensor with shape to int 😔")
             else:
                 return int(self.__storage__)
         
-        def __float__(self):
+        def __float__(self) -> float:
             if len(self.shape) != 0:
                 raise Exception("Cannot convert non-scalar tensor with shape to float 😔")
             else:
@@ -114,7 +126,7 @@ class poortorch:
                     self.__storage__.append(xl)
                 elif isinstance(xl, list):
                     for sub_xl in xl:
-                        self.helper._flatten_list(sub_xl)
+                        poortorch.tensor.helper._flatten_list(self, sub_xl)
                 else:
                     raise Exception("Tensor can only contain int, float or lists 😔")
                 
@@ -148,3 +160,17 @@ class poortorch:
                         shape.append(len(xl))
 
                 return shape
+            
+            def _create_per_value_independent_tensor(shape: tuple[int], value_function: Callable, dtype: "poortorch.dtype") -> "poortorch.tensor":
+                if all(isinstance(dim, int) and dim > 0 for dim in shape): raise Exception("Shape must be a tuple of positive integers 😔")
+                __storage__ = [value_function() for _ in range(math.prod(shape))]
+                if not dtype:
+                    dtype = poortorch.float32 if isinstance(__storage__[0], float) else poortorch.int64
+                
+                manual_creation_dict = {
+                    "__storage__": __storage__,
+                    "dtype": dtype,
+                    "shape": shape,
+                }
+
+                return poortorch.tensor([], None, manual_creation_dict)
